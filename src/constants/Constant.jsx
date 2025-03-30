8c471c300c1bc04c9655a9a5c8440970a41fe573
Anup Parkhi <anupparkhi@Anups-MacBook-Pro-2.local>
2025-03-30 15:06:28 -0500

    Fixed name mismatched

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -8,8 +8,8 @@ import Javascript from "../assets/Javascript.png";
 import Git from "../assets/Git.png";
 import Vite from "../assets/Vite.png";
 import TailwindCSS from "../assets/TailwindCSS.png";
-import react from "../assets/react.png";
-import Vue from "../assets/vue.png";
+import react from "../assets/React.png";
+import Vue from "../assets/Vue.png";
 
 import Collge from "../assets/college.png";
 import Wipro from "../assets/wipro.png";
